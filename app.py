9d24d5b8f071bbc6ddabfaaaefb65482a46f2877
Alex Siqueira <83468184+alexsiqueira15@users.noreply.github.com>
2026-05-04 19:05:59 -0300

    adição das funções TAB para percorrer os caminhos do relatorio

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+import time
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from websocket import send
 
 options = Options()
 options.add_argument('--ignore-certificate-errors')
@@ -44,6 +48,50 @@ navegador.execute_script("arguments[0].click();", consulta)
 submenu = wait.until(EC.presence_of_element_located((By.ID, "iconMenu1_SolpartMenu10306")))
 navegador.execute_script("arguments[0].click();", submenu)
 
+# Garante que algo da página está focado
+body = navegador.find_element(By.TAG_NAME, "body")
+body.click()
+
+# Dá 8 TABs
+for _ in range(2):
+    navegador.switch_to.active_element.send_keys(Keys.TAB)
+    time.sleep(0.2)  # pequeno delay ajuda estabilidade
+
+# Seta para direita
+navegador.switch_to.active_element.send_keys(Keys.ARROW_RIGHT)
+
+for _ in range(5):
+    navegador.switch_to.active_element.send_keys(Keys.TAB)
+    time.sleep(0.2)  # pequeno delay ajuda estabilidade
+
+# Digita o código 194 no campo focado
+navegador.switch_to.active_element.send_keys("194")
+
+for _ in range(6):
+    navegador.switch_to.active_element.send_keys(Keys.TAB)
+    time.sleep(0.2)  # pequeno delay ajuda estabilidade
+        
+navegador.switch_to.active_element.send_keys(Keys.SPACE)
+    
+for _ in range(7):
+    navegador.switch_to.active_element.send_keys(Keys.TAB)
+    time.sleep(0.4)    
+
+# Data de hoje
 
 
-input("Finalizado. Pressione Enter para sair...")
+data_hoje = datetime.now().strftime("%d/%m/%Y")
+
+# Digita no campo
+campo_data = navegador.switch_to.active_element
+campo_data.clear()
+campo_data.send_keys(data_hoje)
+
+navegador.switch_to.active_element.send_keys(Keys.TAB)
+time.sleep(0.2)
+campo_data = navegador.switch_to.active_element
+campo_data.clear()
+campo_data.send_keys(data_hoje)
+
+
+input()
